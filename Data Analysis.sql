c5fbfd1ec925f2225dc635131c6c0734b59de468
anair123 <aashishnair8925@gmail.com>
2022-09-26 22:25:02 -0400

    add data analysis file

---
+++ Data Analysis.sql
@@ -0,0 +1,42 @@
+
+-- get the number of airports
+
+SELECT COUNT(airport_id) AS num_airports FROM Airport;
+
+-- get the number of carriers
+
+SELECT COUNT(carrier_id) AS num_carrier FROM Carrier;
+
+-- find the moving average of total flights 
+
+
+
+-- in what month did each carrier register the most flights? 
+
+-- in what month did each carrier register the least flights?
+
+
+-- in what month did each airport register the most flights? 
+
+-- in what month did each airport register the least flights?
+
+
+-- find the airports that have suffered the most in 2020
+
+-- find the carriers that have suffered the most in 2020
+
+
+
+
+-- compare the average drop in 2020 for large hub, medium hub, and small/non hub airports
+
+-- compare the average rise in flights in 2021/2022 for large hub, medium hub, and small/non hub 
+
+-- find the airports that have bounced back the most in 2021 and 2022
+
+-- compute the total cancelled flights and diverted flights in each year
+
+-- what was the driving cause of delay in each year
+
+-- what was the driving cause of delay in each year for large hub, medium hub, and small/non hub airports
+
